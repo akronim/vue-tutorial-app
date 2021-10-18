fbdaa6e8e4494cc9228b8458ddc1f920e7f1c77e
Marinko Mišković <mmiskovic@hattrick.local>
2021-10-18 08:35:23 +0200

    stubbing components

--- testing_1/tests/unit/mounting-options/Stubs.spec.js
+++ testing_1/tests/unit/mounting-options/Stubs.spec.js
@@ -25,3 +25,50 @@ describe('ParentWithAPICallChild.vue', () => {
     expect(wrapper.findComponent(ComponentWithAsyncCall).exists()).toBe(true)
   })
 })
+
+describe('stubbing components', () => {
+  it('stubs dynamic components', () => {
+    const Foo = {
+      name: 'Foo',
+      template: '<div>Foo</div>'
+    }
+    const Bar = {
+      name: 'Bar',
+      template: '<div>Bar</div>'
+    }
+    const Faz = {
+      name: 'Faz',
+      template: '<div>Faz</div>'
+    }
+    const Blah = {
+      name: 'Blah',
+      template: '<div>Blah</div>'
+    }
+    const TestComponent = {
+      template: `
+      <div>
+        <Foo />
+        <Bar />
+        <Blah />
+      </div>
+      `,
+      components: { Foo, Bar, Faz, Blah }
+    }
+    const wrapper = mount(TestComponent, {
+      stubs: {
+        Foo: true,
+        Bar: { template: '<div class="stubbed-bar" />' },
+        Blah: Faz
+      }
+    })
+    console.log(wrapper.html())
+    expect(wrapper.findComponent(Foo).exists()).to.be.true
+
+    expect(wrapper.find('.stubbed-bar').exists()).to.be.true
+    expect(wrapper.findComponent(Bar).exists()).to.be.true
+
+    expect(wrapper.findComponent(Blah).exists()).not.to.be.true
+
+    expect(wrapper.findComponent(Faz).exists()).to.be.true
+  })
+})
